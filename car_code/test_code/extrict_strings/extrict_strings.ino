3f5d61899bfa1b69c5745c25aac7d44e173f27a4
unknown <>
2016-06-16 22:13:06 +0800

    写了一点点

--- car_code/test_code/extrict_strings/extrict_strings.ino
+++ car_code/test_code/extrict_strings/extrict_strings.ino
@@ -1,12 +1,9 @@
-#define SERIAL_TX_BUFFER_SIZE 128//修改串口缓存区大小
-#define SERIAL_RX_BUFFER_SIZE 128//修改串口缓存区大小
-#include <stdio.h>
-#include <string.h>
-
-int i;   // 保存PWM需要输出的值
-String inString = "";   // 输入的字符串
-char LED = ' ';   // 用于判断指定LED颜色对应的引脚
-boolean stringComplete = false; // 用于判断数据是否读取完成
+//#define SERIAL_TX_BUFFER_SIZE 128//修改串口缓存区大小
+//#define SERIAL_RX_BUFFER_SIZE 128//修改串口缓存区大小
+//#include <stdio.h>
+//#include <string.h>
+int obs1, obs2;
+String comdata = "";   // 输入的字符串
 void setup()
 {
   Serial.begin(115200);
@@ -14,18 +11,25 @@ void setup()
 
 void loop()
 {
+  int i, j;
   while (Serial.available() > 0)
   {
-    char data = Serial.read();
+    comdata += Serial.read();
     delay(2);
   }
-  if (isDigit(inChar))
+  char data[comdata.length()];
+  for ( i = 0; i < comdata.length(); i++)
   {
-inString += inChar;
-
+    data[i] = comdata[i];
   }
-  //char str[comdata.length()];
-  // n = atoi(comdata);
+  data[i] = '\0';
+  for (j = 0; j < comdata.length(); j++)
+  {
+    if (isDigit(data[j]))
+    {
+
+    }
+  }
 }
 
 
