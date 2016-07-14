a8cc521bc6cddf15355c53527f2411164ab20e85
unknown <>
2016-07-14 15:42:21 +0800

    update serial_US_B16_test.ino

--- car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
+++ car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
@@ -2,14 +2,10 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3); //用2，3作为虚拟串口（RX，TX）
 
-String data = "";//字符串变量，赋空值
-int  obstacle_number = 0x60; //分别对应超声波1~8
-int long uart_data[8] = {0};
-
+String comdata = "";//字符串变量，赋空值
+int  obstacle_number = 60; //分别对应超声波1~8
 int serial_tag = 0;//标记串口是否被读取
-
 int obstacle_1;/*超声波1距离*/
-unsigned char Re_buf[11], counter = 0;
 void setup()
 {
   delay(200);//延时一会等供电稳定
@@ -21,18 +17,19 @@ void loop()
   mySerial.print("T");//模块测距命令
   mySerial.write(obstacle_number);//模块地址
 
-  while (mySerial.available())
+  int j = 0;
+  while (mySerial.available() > 0)
   {
-    Re_buf[counter] = (unsigned char)mySerial.read();
-    if (counter == 0 ) return; // 检查帧头
-    counter++;
-    if (counter == 11)             //接收到数据
-    {
-      counter = 0;               //重新赋值，准备下一帧数据的接收
-    }
+    comdata += char(mySerial.read());
+    delay(2);
+    serial_tag = 1;
   }
-  if (Re_buf[0] == 0x5A && Re_buf[2] == 0x15 ) //检查帧头，帧尾数据是否符合（0x15）
+  if (serial_tag = 1)
   {
-
+    obstacle_1 = comdata[1] * 1000 + comdata[2] * 100 + comdata[3] * 10 + comdata[4];
   }
+  comdata = String("");
+  serial_tag = 0;
+  Serial.println("obstacle_1");
+  Serial.println(obstacle_1);
 }
