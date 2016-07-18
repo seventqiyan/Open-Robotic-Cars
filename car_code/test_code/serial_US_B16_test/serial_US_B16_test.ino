b489bcd0f40ac34ef008d4db4b3493e8a6a88bb3
unknown <>
2016-07-18 20:10:53 +0800

    no message

--- car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
+++ car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
@@ -1,6 +1,6 @@
 
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3); //用2，3作为虚拟串口（RX，TX）
+//#include <SoftwareSerial.h>
+//SoftwareSerial Serial1(2, 3); //用2，3作为虚拟串口（RX，TX）
 
 String comdata = "";//字符串变量，赋空值
 int  obstacle_number = 60; //分别对应超声波1~8
@@ -10,26 +10,28 @@ void setup()
 {
   delay(200);//延时一会等供电稳定
   Serial.begin(115200);//串口通信波特率
-  mySerial.begin(115200);//虚拟串口波特率，不稳定则往下调整
+  Serial1.begin(115200);//虚拟串口波特率，不稳定则往下调整
 }
 void loop()
 {
-  mySerial.print("T");//模块测距命令
-  mySerial.write(obstacle_number);//模块地址
+ Serial.write(0x54);//模块测距命令
+    Serial.write(0x60);//模块地址
 
-  int j = 0;
-  while (mySerial.available() > 0)
+ // int j = 0;f
+  while (Serial1.available() > 0)
   {
-    comdata += char(mySerial.read());
+    comdata += char(Serial1.read());
     delay(2);
-    serial_tag = 1;
+   // serial_tag = 1;
   }
-  if (serial_tag = 1)
+ /* if (serial_tag = 1)
   {
     obstacle_1 = comdata[1] * 1000 + comdata[2] * 100 + comdata[3] * 10 + comdata[4];
-  }
-  comdata = String("");
-  serial_tag = 0;
+  }*/
+ 
+//  serial_tag = 0;
   Serial.println("obstacle_1");
-  Serial.println(obstacle_1);
+  Serial.println(comdata);
+  delay(500); 
+  comdata = String("");
 }
