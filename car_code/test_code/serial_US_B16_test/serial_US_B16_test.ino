9176590dabae389e4a518a49911d59f277e762ed
unknown <>
2016-07-21 20:52:21 +0800

    update serial_US_B16.ino

--- car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
+++ car_code/test_code/serial_US_B16_test/serial_US_B16_test.ino
@@ -3,8 +3,7 @@
 //SoftwareSerial Serial1(2, 3); //用2，3作为虚拟串口（RX，TX）
 
 String comdata = "";//字符串变量，赋空值
-int  obstacle_number = 60; //分别对应超声波1~8
-int serial_tag = 0;//标记串口是否被读取
+//int  obstacle_number = 60; //分别对应超声波1~8
 int obstacle_1;/*超声波1距离*/
 void setup()
 {
@@ -14,24 +13,17 @@ void setup()
 }
 void loop()
 {
- Serial.write(0x54);//模块测距命令
-    Serial.write(0x60);//模块地址
-
- // int j = 0;f
+  delay(100);
+  Serial1.write(0x54);//模块测距命令
+  Serial1.write(0x60);//模块地址
+ 
   while (Serial1.available() > 0)
   {
     comdata += char(Serial1.read());
     delay(2);
-   // serial_tag = 1;
-  }
- /* if (serial_tag = 1)
-  {
-    obstacle_1 = comdata[1] * 1000 + comdata[2] * 100 + comdata[3] * 10 + comdata[4];
-  }*/
  
-//  serial_tag = 0;
-  Serial.println("obstacle_1");
+  Serial.print("obstacle_1: ");
   Serial.println(comdata);
-  delay(500); 
+  delay(500);
   comdata = String("");
 }
